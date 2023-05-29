cb96b8d8d616151551933464b1bd2b2f0368c8f1
lasconic <lasconic@gmail.com>
2023-05-29 23:12:21 +0200

    add some tests

--- check.sh
+++ check.sh
@@ -2,6 +2,6 @@
 #
 # Small script to ensure quality checks pass before submitting a commit/PR.
 #
-python -m black finary_uapi
-python -m flake8 finary_uapi
-python -m mypy finary_uapi
+python -m black finary_uapi tests
+python -m flake8 finary_uapi tests
+python -m mypy finary_uapi tests
---
+++ tests/test_get.py
@@ -0,0 +1,80 @@
+import pytest
+import requests
+from importlib import import_module
+from finary_uapi.signin import signin
+from finary_uapi.auth import prepare_session
+
+
+def test_signin() -> None:
+    """test signin"""
+    result = signin()
+    assert result["message"] == "Created"
+    assert result["error"] is None
+
+
+@pytest.fixture
+def session() -> requests.Session:
+    return prepare_session()
+
+
+@pytest.mark.parametrize(
+    "module_name, function_name, args",
+    [
+        ("bank_account_types", "", {"type": "invest"}),
+        ("bank_account_types", "", {"type": "cash"}),
+        ("crypto_chains", "", {}),
+        ("currencies", "", {"type": "crypto", "query": "BTC"}),
+        ("currencies", "", {"type": "fiat", "query": "USD"}),
+        ("generic_asset_categories", "", {}),
+        ("institutions", "", {"name": "cred"}),
+        ("precious_metals", "", {"query": "or"}),
+        ("scpis", "", {"query": "corum"}),
+        ("securities", "", {"query": "GOOG"}),
+        ("user_cryptos", "", {}),
+        ("user_fonds_euro", "", {}),
+        ("user_generic_assets", "", {}),
+        (
+            "user_holdings_accounts",
+            "get_holdings_accounts",
+            {},
+        ),  # TODO change name and add _user_
+        ("user_me", "", {}),
+        ("user_me", "get_user_me_institution_connections", {}),
+        ("user_portfolio", "get_portfolio_crowdlendings", {}),
+        ("user_portfolio", "get_portfolio_crowdlendings_distribution", {}),
+        ("user_portfolio", "get_portfolio_cryptos", {}),
+        ("user_portfolio", "get_portfolio_cryptos_distribution", {}),
+        ("user_portfolio", "get_portfolio_investments", {}),
+        ("user_precious_metals", "", {}),
+        ("user_real_estates", "", {}),
+        ("user_scpis", "", {}),
+        ("user_securities", "", {}),
+        ("user_startups", "", {}),
+        ("views", "get_dashboard", {"type": "net", "period": "all"}),
+        ("views", "get_dashboard", {"type": "gross", "period": "all"}),
+        ("views", "get_dashboard", {"type": "finary", "period": "all"}),
+        ("views", "get_portfolio", {"period": "all"}),
+        ("views", "get_savings_accounts", {"period": "all"}),
+        ("views", "get_checking_accounts", {"period": "all"}),
+        ("views", "get_other_assets", {"period": "all"}),
+        ("views", "get_fonds_euro", {"period": "all"}),
+        ("views", "get_real_estates", {"period": "all"}),
+        ("views", "get_commodities", {"period": "all"}),
+        ("views", "get_insights", {}),
+        ("views", "get_fees", {}),
+        ("views", "get_loans", {}),
+        ("views", "get_credit_accounts", {}),
+        ("watches", "", {"query": "rolex"}),
+    ],
+)
+def test_generic_test(
+    session: requests.Session, module_name: str, function_name: str, args
+) -> None:
+    if not function_name:
+        function_name = f"get_{module_name}"
+    module = import_module(f"finary_uapi.{module_name}")
+    bar = getattr(module, function_name)
+    result = bar(session, **args)
+    assert result["message"] == "OK"
+    assert result["error"] is None
+    assert len(result["result"]) > 0
