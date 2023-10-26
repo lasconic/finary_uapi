a2343af02cb67d0574fedc9f127045af756366f7
Nicolas Froment <lasconic@gmail.com>
2023-10-26 16:05:58 +0200

    fix #131: implement a function to call users/me/organizations (#134)
    * fix #131: implement a function to call users/me/organizations

--- finary_uapi/user_me.py
+++ finary_uapi/user_me.py
@@ -20,6 +20,11 @@ def get_user_me_sharing_links(session: requests.Session) -> Any:
     return get_and_print(session, url)
 
 
+def get_user_me_organizations(session: requests.Session) -> Any:
+    url = f"{API_ROOT}/users/me/organizations"
+    return get_and_print(session, url)
+
+
 def get_user_me_subscription_details(session: requests.Session) -> Any:
     url = f"{API_ROOT}/users/me/subscription_details"
     return get_and_print(session, url)
