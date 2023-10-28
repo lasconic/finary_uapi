e6689f0f84257ba0b19e0a8561be72aaac0bfc44
NickFR <20896232+nmathey@users.noreply.github.com>
2023-10-28 12:39:36 +1100

    handling currency for UI display + real estate

--- finary_uapi/currencies.py
+++ finary_uapi/currencies.py
@@ -30,3 +30,31 @@ def get_cryptocurrency_by_code(session: requests.Session, code: str):
                 return currency
         return currency
     return {}
+
+def get_display_currency_code(session: requests.Session):
+    url = f"{API_ROOT}/users/me"
+    x = session.get(url)
+    display_currency_code = x.json()
+    return display_currency_code["result"]["ui_configuration"]["display_currency"]["code"]
+
+def update_display_currency_by_code(session: requests.Session, code: str):
+    """
+    code = ISO 4217 three-letter code
+    Currently limited by Finary to "EUR", "USD", "SGD", "CHF", "GBP",or "CAD"
+    """ 
+    url = f"{API_ROOT}/users/me"
+    data = {
+        "ui_configuration": {
+            "display_currency":{
+                "code": code
+            }
+        }
+    }
+    data_json = json.dumps(data)
+    headers = {}
+    headers["Content-Length"] = str(len(data_json))
+    headers["Content-Type"] = "application/json"
+    x = session.put(url, data=data_json, headers=headers)
+    logging.debug(x.status_code)
+    logging.debug(json.dumps(x.json(), indent=4))
+    return x.json()
