5961f1272fc85bac2789223f29cdb912332894ec
Nicolas Froment <lasconic@gmail.com>
2023-10-30 15:57:29 +0100

    Get fiatcurrency by code (#142)
    * add get_fiatcurrency_by_code function
    
    * add tests
    
    ---------
    
    Co-authored-by: NickFR <20896232+nmathey@users.noreply.github.com>

--- finary_uapi/currencies.py
+++ finary_uapi/currencies.py
@@ -21,8 +21,13 @@ def get_currencies(session: requests.Session, type: str, query: str):
 
 
 # utils functions
-def get_cryptocurrency_by_code(session: requests.Session, code: str):
-    currencies = get_currencies(session, "crypto", code)
+
+
+def get_currency_by_code(session: requests.Session, type: str, code: str):
+    """
+    type = 'crypto' or 'fiat'
+    """
+    currencies = get_currencies(session, type, code)
     if len(currencies["result"]) > 0:
         currency = currencies["result"][0]
         for currency in currencies["result"]:
@@ -30,3 +35,11 @@ def get_cryptocurrency_by_code(session: requests.Session, code: str):
                 return currency
         return currency
     return {}
+
+
+def get_cryptocurrency_by_code(session: requests.Session, code: str):
+    return get_currency_by_code(session, "crypto", code)
+
+
+def get_fiatcurrency_by_code(session: requests.Session, code: str):
+    return get_currency_by_code(session, "fiat", code)
